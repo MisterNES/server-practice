dfa5172ec6d93596d20fa5d99c35c2376b43a135
Joe Kim <joekim6a@gmail.com>
2021-04-12 14:19:35 -0700

    complete phase 2a

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 http.createServer(function(req, res) {
-    res.end();
+    if (req.url === "/OK") {
+        console.log("Inbound 'OK' request being processed...");
+        res.writeHead(200);
+        res.end();
+    } else {
+        console.log("404 Not Found");
+        res.writeHead(404);
+        res.end();
+    }
 }).listen(3000, function() {
     console.log("Listening for request on port 3000...");
 });
